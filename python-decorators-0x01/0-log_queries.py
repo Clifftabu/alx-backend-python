4f3d8b72430aa8e8cba45abd98d0594c020a878e
CLIFFTABU <clifftabu@gmail.com>
2025-05-24 16:05:49 +0300

    solo commit again

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,15 +1,17 @@
 import sqlite3
 import functools
+from datetime import datetime  
 
-
+# ✅ Decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        query = kwargs.get('query') or (args[0] if args else None)
-        print(f"[LOG] Executing SQL Query: {query}")
+        query = kwargs.get('query') or (args[0] if args else '')
+        print(f"[{datetime.now()}] Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
+# ✅ Function to fetch all users, 
 @log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
@@ -19,6 +21,6 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-# Fetch users while logging the query
+# ✅ Execute
 users = fetch_all_users(query="SELECT * FROM users")
 print(users)
