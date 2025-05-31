09ca4cd8e178063987598183348d2f548e953367
CLIFFTABU <clifftabu@gmail.com>
2025-05-31 22:26:03 +0300

    commit

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
+from rest_framework import filters
+
 
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer
