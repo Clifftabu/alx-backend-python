3122d3737491dbd2afac855a3eedb9f961169d26
CLIFFTABU <clifftabu@gmail.com>
2025-06-08 23:29:53 +0300

    6th commit

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,4 +1,6 @@
 import logging
+import datetime
+from django.http import HttpResponse
 
 
 logging.basicConfig(
@@ -8,11 +10,25 @@ logging.basicConfig(
 )
 
 
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_time = datetime.datetime.now().time()
+        if current_time.hour < 9 or current_time.hour >= 17:
+            return HttpResponse("Access denied: allowed between 9 AM and 5 PM.")
+        return self.get_response(request)
+
+
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        logging.info(f"{request.method} request to {request.path}")
-        response = self.get_response(request)
-        return response
+        method = request.method
+        path = request.get_full_path()
+        with open("requests.log", "a") as f:
+            f.write(f"{datetime.datetime.now()} - {method} {path}\n")
+        return self.get_response(request)
+
