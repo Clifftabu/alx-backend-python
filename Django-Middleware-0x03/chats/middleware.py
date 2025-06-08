c8f18ac183d9b65b2ea32842d871051ce37d5f6e
CLIFFTABU <clifftabu@gmail.com>
2025-06-08 22:41:51 +0300

    After correction of Task 0

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,1 +1,13 @@
- 
+from datetime import datetime
+from django.http import HttpResponse
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        if current_hour < 9 or current_hour >= 17:
+            return HttpResponse("Access restricted to working hours (9 AM - 5 PM).", status=403)
+        return self.get_response(request)
