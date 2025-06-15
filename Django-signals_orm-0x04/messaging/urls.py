13d2954515260e0e28f02f44cfa4098f56c1bd55
CLIFFTABU <clifftabu@gmail.com>
2025-06-15 23:34:42 +0300

    final commit

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -1,8 +1,10 @@
 # messaging/urls.py
 from django.urls import path
 from . import views
+from .views import unread_messages_view
 
 urlpatterns = [
     path('delete_user/', views.delete_user, name='delete_user'),
     path('threaded/', views.threaded_conversations, name='threaded_conversations'),
+    path('unread/', unread_messages_view, name='unread-messages'),
 ]
