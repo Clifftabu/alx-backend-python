b18805d884874e3e338aaab7572e2e15617899ef
CLIFFTABU <clifftabu@gmail.com>
2025-06-15 22:40:31 +0300

    task 3 commit

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('delete_user/', views.delete_user, name='delete_user'),
+    path('threaded/', views.threaded_conversations, name='threaded_conversations'),
 ]
